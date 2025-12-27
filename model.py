de5ff85b5b57830cd09b391a3057b8f5e6cf1c25
ipranaysatija <pranaysatija01@gmail.com>
2025-12-28 00:28:07 +0530

    removed api keys

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ def maze_ai(query):
     llm = ChatCohere(
         model="command-r-08-2024",
         temperature=0,
-        cohere_api_key="S7NonwWcXFTLqxiHwqbtpT7HcZchJ0MA87W90XYS",
+        cohere_api_key="YOUR _API_KEY_HERE",
         max_tokens=10,
         )
     return llm.invoke(query).content
